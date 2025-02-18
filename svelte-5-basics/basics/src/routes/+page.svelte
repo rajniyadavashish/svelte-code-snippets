cce650c0ab40f8277f407ee1a9da2ba5517ddad0
Ashish <your.email@example.com>
2025-02-18 08:17:49 +0530

    basic changes

--- svelte-5-basics/basics/src/routes/+page.svelte
+++ svelte-5-basics/basics/src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	function onClick() {
+		console.log('The button has been clicked');
+	}
+  let num = 5;
+</script>
+
+<button onclick={onClick}>Click me</button>
+
+<h1>{num}</h1>
+
+<div class="container">
+  <p>this is left side</p>
+  <p>this is right side</p>
+</div>
+
+
+<style>
+  h1 {
+    color: blue;
+  }
+
+  .container {
+    display: flex;
+    justify-content: space-between;
+  }
+</style>
