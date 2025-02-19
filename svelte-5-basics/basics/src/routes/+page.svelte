a1cac486a509cb3794e18ad41d3c1c719c1a23f8
Ashish <your.email@example.com>
2025-02-19 22:50:06 +0530

    added derived rune example

--- svelte-5-basics/basics/src/routes/+page.svelte
+++ svelte-5-basics/basics/src/routes/+page.svelte
@@ -1,14 +1,19 @@
 <script lang="ts">
+	// state rune
+	let num = $state(0);
+	// derived rune
+	let userInfo = $derived(num === 0 ? 'Please click the button' : `you clicked ${num} times`);
+	// we can also use functions in derived runes
+
 	function onClick() {
-		console.log('The button has been clicked');
+		num++;
 	}
-	let num = 5;
 </script>
 
 <button onclick={onClick}>Click me</button>
 
 <h1>{num}</h1>
-
+<p>{userInfo}</p>
 <div class="container">
 	<p>this is left side</p>
 	<p>this is right side</p>
